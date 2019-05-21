b8963124a33ef50f977f9c4b8c50b3ba4f4d5572
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-05-21 19:28:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(str)
-  if str == str.upcase
-    return ""
+  if str != str.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif str == str.upcase
+    return 
 end  
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
